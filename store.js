90118aa21eac6521b7dd8ee548a3e840361d9da1
themartiandev <61661678+themartiandev@users.noreply.github.com>
2020-05-19 10:28:36 +0530

    updating store middleware

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import AllReducers from "./src/reducers";
 import createSagaMiddeleware from "redux-saga";
-import {decode as atob, encode as btoa} from 'base-64';
+import { decode as atob, encode as btoa } from "base-64";
 import { LOGIN_SUC, FETCH_NEWS_ACTION } from "./src/actions//ActionTypes";
 import { setToken, getToken } from "./src/apis/api";
 
@@ -21,11 +21,14 @@ const checkExpiredToken = (store) => (next) => (action) => {
   if (action.type === FETCH_NEWS_ACTION) {
     let currToken = getToken();
     let currTokenObj = JSON.parse(atob(currToken.split(".")[1]));
-    let now=Date.now().valueOf() / 1000;
-    if (typeof currTokenObj.exp !== 'undefined' && currTokenObj.exp < now) {
-      console.log('token is expired')
-    }else if(typeof currTokenObj.exp !== 'undefined' && currTokenObj.exp > now){
-      console.log('token is not expired')
+    let now = Date.now().valueOf() / 1000;
+    if (typeof currTokenObj.exp !== "undefined" && currTokenObj.exp < now) {
+      console.log("token is expired");
+    } else if (
+      typeof currTokenObj.exp !== "undefined" &&
+      currTokenObj.exp > now
+    ) {
+      console.log("token is not expired");
     }
   }
   return next(action);
